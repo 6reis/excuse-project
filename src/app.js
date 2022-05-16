5dfad37dd77924a0dc3ae33754f6402747efb789
6reis <Gracenunezr@gmail.com>
2022-05-16 16:55:11 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function excuseContructor(num) {
+    document.getElementById("excuse").innerHTML =
+      who[num] + " " + what[num] + " " + when[num];
+    //document.getElementById("excuse").innerHTML = "LALA";
+  }
+
+  function getNumberRamdom() {
+    return Math.floor(Math.random() * (4 - 0)) + 0;
+  }
+  console.log(getNumberRamdom());
+
+  let ramdomNumber = getNumberRamdom();
+
+  excuseContructor(ramdomNumber);
+
+  console.log(who);
+};
